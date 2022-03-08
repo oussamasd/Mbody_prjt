e413aa44df3c1330ac0c6f754bb4fda9834b8b30
ilyesnakhli <61388882+ilyesnakhli@users.noreply.github.com>
2022-03-08 12:18:24 +0100

    Add files via upload

---
+++ src/Entity/TypeReclamation.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TypeReclamationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=TypeReclamationRepository::class)
+ */
+class TypeReclamation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     * @Assert\NotBlank(message=" ce champ est obligatoire")
+     */
+    private $nom;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Reclamation::class, mappedBy="typeReclamation",cascade={"remove"},)
+     */
+    private $reclamations;
+
+    public function __construct()
+    {
+        $this->reclamations = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Reclamation[]
+     */
+    public function getReclamations(): Collection
+    {
+        return $this->reclamations;
+    }
+
+    public function addReclamation(Reclamation $reclamation): self
+    {
+        if (!$this->reclamations->contains($reclamation)) {
+            $this->reclamations[] = $reclamation;
+            $reclamation->setTypeReclamation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReclamation(Reclamation $reclamation): self
+    {
+        if ($this->reclamations->removeElement($reclamation)) {
+            // set the owning side to null (unless already changed)
+            if ($reclamation->getTypeReclamation() === $this) {
+                $reclamation->setTypeReclamation(null);
+            }
+        }
+
+        return $this;
+    }
+}
