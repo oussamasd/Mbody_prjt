cbfb53beca77642aeb8ad4d1434685142542a3ca
ilyesnakhli <61388882+ilyesnakhli@users.noreply.github.com>
2022-03-08 12:16:44 +0100

    Add files via upload

---
+++ migrations/Version20220215142249.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220215142249 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE reclamation ADD type_reclamation_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE reclamation ADD CONSTRAINT FK_CE6064047BA88B4D FOREIGN KEY (type_reclamation_id) REFERENCES type_reclamation (id)');
+        $this->addSql('CREATE INDEX IDX_CE6064047BA88B4D ON reclamation (type_reclamation_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE reclamation DROP FOREIGN KEY FK_CE6064047BA88B4D');
+        $this->addSql('DROP INDEX IDX_CE6064047BA88B4D ON reclamation');
+        $this->addSql('ALTER TABLE reclamation DROP type_reclamation_id, CHANGE nom nom VARCHAR(20) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE description description LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE type_reclamation CHANGE nom nom VARCHAR(20) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
